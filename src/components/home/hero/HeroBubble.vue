209140d44d27c19615f1328ef7c35ba1d775fff6
Tenotea <drtenotea@gmail.com>
2021-05-05 02:17:33 +0100

    Updated the css vars to an any type. This should technically stop the compile error

--- src/components/home/hero/HeroBubble.vue
+++ src/components/home/hero/HeroBubble.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <div id="bubble" :size="size" :style="{'--size': size+'px' }">
+    <div id="bubble" :size="size" :style="cssVars">
 
     </div>
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import { defineComponent } from 'vue'
 
 export default defineComponent({
@@ -14,6 +14,14 @@ export default defineComponent({
     size: {
       type: String,
       default: '23'
+    }
+  },
+
+  computed: {
+    cssVars ():any {
+      return {
+      '--size': this.size + 'px'
+      }
     }
   }
 })
